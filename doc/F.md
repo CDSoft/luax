d67ac368fa15b9feec7faab1ecb099acfd7b9d0e
Christophe Delord <cdelord.fr>
2023-11-11 00:06:08 +0100

    `F.case` simplified, `F.when` removed

--- doc/F.md
+++ doc/F.md
@@ -108,34 +108,15 @@ F.default(def, x)
 
 ``` lua
 F.case(x) {
-    { t1, v1 },
+    t1 = v1,
     ...
-    { tn, vn }
+    tn = vn,
+    [otherwise = default_value]
 }
 ```
 
-> returns the first `vi` such that `ti == x`. If `ti` is a function, it
-> is applied to `x` and the test becomes `ti(x) == x`. If `vi` is a
-> function, the value returned by `F.case` is `vi(x)`.
-
-``` lua
-F.when {
-    { t1, v1 },
-    ...
-    { tn, vn }
-}
-```
-
-> returns the first `vi` such that `ti` is true. If `ti` is a function,
-> the test becomes `ti()`. If `vi` is a function, the value returned by
-> `F.when` is `vi()`.
-
-``` lua
-F.otherwise
-```
-
-> `F.otherwise` is used with `F.case` and `F.when` to add a default
-> branch.
+> returns `vi` such that `ti == x` or `default_value` if no `ti` is
+> equal to `x`.
 
 #### Tuples
 
--- luax-libs/F/F.lua
+++ luax-libs/F/F.lua
@@ -354,65 +354,20 @@ end
 --[[@@@
 ```lua
 F.case(x) {
-    { t1, v1 },
+    t1 = v1,
     ...
-    { tn, vn }
+    tn = vn,
+    [otherwise = default_value]
 }
 ```
-> returns the first `vi` such that `ti == x`.
-If `ti` is a function, it is applied to `x` and the test becomes `ti(x) == x`.
-If `vi` is a function, the value returned by `F.case` is `vi(x)`.
+> returns `vi` such that `ti == x` or `default_value` if no `ti` is equal to `x`.
 @@@]]
-
-local otherwise = setmetatable({}, {
-    __call = function(_) return true end,
-    __tostring = function(_) return "otherwise" end,
-})
 
 function F.case(val)
     return function(cases)
-        for i = 1, #cases do
-            local test, res = table.unpack(cases[i])
-            if type(test) == "function" then test = test(val) end
-            if val == test or rawequal(test, otherwise) then
-                if type(res) == "function" then res = res(val) end
-                return res
-            end
-        end
+        return F.default(cases.otherwise, cases[val])
     end
 end
-
---[[@@@
-```lua
-F.when {
-    { t1, v1 },
-    ...
-    { tn, vn }
-}
-```
-> returns the first `vi` such that `ti` is true.
-If `ti` is a function, the test becomes `ti()`.
-If `vi` is a function, the value returned by `F.when` is `vi()`.
-@@@]]
-
-function F.when(cases)
-    for i = 1, #cases do
-        local test, res = table.unpack(cases[i])
-        if type(test) == "function" then test = test() end
-        if test then
-            if type(res) == "function" then res = res() end
-            return res
-        end
-    end
-end
-
---[[@@@
-```lua
-F.otherwise
-```
-> `F.otherwise` is used with `F.case` and `F.when` to add a default branch.
-@@@]]
-F.otherwise = otherwise
 
 --[[------------------------------------------------------------------------@@@
 #### Tuples
