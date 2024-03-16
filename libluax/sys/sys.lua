cf3a19c1e655c7a3dab177da74ad0b64541a4f97
Christophe Delord <cdelord.fr>
2024-03-16 22:42:46 +0100

    better platform detection in sys.lua
    - use package.cpath to identify the OS
    - use `uname -m` on Linux/MacOS and `%PROCESSOR_ARCHITECTURE%` on Windows
      to identify the processor architecture

--- libluax/sys/sys.lua
+++ libluax/sys/sys.lua
@@ -27,65 +27,38 @@ sys = _ and sys or {
 local F = require "F"
 
 local targets = F{
-    {name="linux-x86_64",       uname_kernel="Linux",  uname_machine="x86_64",  os="linux",   arch="x86_64",  libc="gnu",   exe="",     so=".so"   },
-    {name="linux-x86_64-musl",  uname_kernel="Linux",  uname_machine="x86_64",  os="linux",   arch="x86_64",  libc="musl",  exe="",     so=".so"   },
-    {name="linux-aarch64",      uname_kernel="Linux",  uname_machine="aarch64", os="linux",   arch="aarch64", libc="gnu",   exe="",     so=".so"   },
-    {name="linux-aarch64-musl", uname_kernel="Linux",  uname_machine="aarch64", os="linux",   arch="aarch64", libc="musl",  exe="",     so=".so"   },
-    {name="macos-x86_64",       uname_kernel="Darwin", uname_machine="x86_64",  os="macos",   arch="x86_64",  libc="none",  exe="",     so=".dylib"},
-    {name="macos-aarch64",      uname_kernel="Darwin", uname_machine="arm64",   os="macos",   arch="aarch64", libc="none",  exe="",     so=".dylib"},
-    {name="windows-x86_64",     uname_kernel="MINGW",  uname_machine="x86_64",  os="windows", arch="x86_64",  libc="gnu",   exe=".exe", so=".dll"  },
+    {name="linux-x86_64",       uname_machine="x86_64",  os="linux",   arch="x86_64",  libc="gnu",   exe="",     so=".so"   },
+    {name="linux-x86_64-musl",  uname_machine="x86_64",  os="linux",   arch="x86_64",  libc="musl",  exe="",     so=".so"   },
+    {name="linux-aarch64",      uname_machine="aarch64", os="linux",   arch="aarch64", libc="gnu",   exe="",     so=".so"   },
+    {name="linux-aarch64-musl", uname_machine="aarch64", os="linux",   arch="aarch64", libc="musl",  exe="",     so=".so"   },
+    {name="macos-x86_64",       uname_machine="x86_64",  os="macos",   arch="x86_64",  libc="none",  exe="",     so=".dylib"},
+    {name="macos-aarch64",      uname_machine="arm64",   os="macos",   arch="aarch64", libc="none",  exe="",     so=".dylib"},
+    {name="windows-x86_64",     uname_machine="AMD64",   os="windows", arch="x86_64",  libc="gnu",   exe=".exe", so=".dll"  },
 }
 targets : foreach(function(target) targets[target.name] = target end)
 
 if sys.libc == "lua" then
 
-    if pandoc then
+    local function uname() return io.popen("uname -m", "r") : read "a" : trim() end
 
-        sys.os   = pandoc.system.os
-        sys.arch = pandoc.system.arch
+    -- the libraries extension in package.cpath is specific to the OS
+    sys.so = package.cpath:match "%.[^%.]-$"
+    sys.os = assert(targets : find(function(t) return t.so == sys.so end), "Unknown OS").os
 
-        if sys.os:match"mingw" then sys.os = "windows" end
+    sys.arch = pandoc and pandoc.system.arch or
+        (function()
+            local machine = F.case(sys.os) {
+                linux   = uname,
+                macos   = uname,
+                windows = function() return os.getenv "PROCESSOR_ARCHITECTURE" end,
+            }()
+            return assert(targets : find(function(t) return t.os==sys.os and t.uname_machine==machine end), "Unknown architecture").arch
+        end)()
 
-        targets : foreach(function(target)
-            if not sys.name and sys.os == target.os and sys.arch == target.arch then
-                sys.exe  = target.exe
-                sys.so   = target.so
-                sys.name = target.name
-            end
-        end)
+    local host = assert(targets : find(function(t) return t.os==sys.os and t.arch==sys.arch end), "Unknown platform")
 
-    else
-
-        local sh = require "sh"
-        local os, arch = F(sh.read"uname -s -m" or "")
-                            : words() ---@diagnostic disable-line: undefined-field
-                            : unpack()
-        local host = targets
-            : filter(function(target) return os:match(target.uname_kernel) and arch:match(target.uname_machine) end)
-            : head()
-        if host then
-            -- Linux/MacOS detected with uname
-            sys.os   = host.os
-            sys.arch = host.arch
-            sys.exe  = host.exe
-            sys.so   = host.so
-            sys.name = host.name
-        else
-            local win_os = os.getenv "OS"
-            if win_os:match "Windows" then
-                -- Assuming Windows
-                local win = targets
-                    : filter(function(target) return target.os=="windows" end)
-                    : head()
-                sys.os   = win.os
-                sys.arch = win.arch
-                sys.exe  = win.exe
-                sys.so   = win.so
-                sys.name = win.name
-            end
-        end
-
-    end
+    sys.exe  = host.exe
+    sys.name = host.name
 
 end
 
