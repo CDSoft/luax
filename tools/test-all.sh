e5801bf539180ec4cb4871497feecabfd6add24b
Christophe Delord <cdelord.fr>
2024-08-03 16:31:41 +0200

    script to test all compilation configurations

---
+++ tools/test-all.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# This file is part of luax.
+#
+# luax is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# luax is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with luax.  If not, see <https://www.gnu.org/licenses/>.
+#
+# For further information about luax you can visit
+# http://cdelord.fr/luax
+
+set -e
+
+cd "$(git rev-parse --show-toplevel)"
+
+check()
+{
+    ./bootstrap.sh "$@" && ninja all
+}
+
+check fast zig
+check fast zig strip
+check fast gcc
+check fast gcc strip
+check fast clang
+check fast clang strip
+
+check small zig
+check small zig strip
+check small gcc
+check small gcc strip
+check small clang
+check small clang strip
+
+check debug zig
+check debug zig strip
+check debug gcc
+check debug gcc strip
+check debug clang
+check debug clang strip
+
+check debug san
+check debug san strip
