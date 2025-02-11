81db9db2543eb1631bd96ee1ebd440f07c452848
Christophe Delord <CDSoft.github.io>
2025-02-11 22:17:36 +0100

    remove useless script path in tools/luax.lua

--- tools/luax.lua
+++ tools/luax.lua
@@ -39,8 +39,6 @@ package.path = table.concat({
     "libluax/sys/?.lua",
     "libluax/term/?.lua",
     "libluax/lar/?.lua",
-    "libluax/lz4/?.lua",
-    "libluax/lzip/?.lua",
 
     "ext/lua/argparse/?.lua",
     "ext/lua/cbor/?.lua",
