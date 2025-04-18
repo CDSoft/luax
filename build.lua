8adee0f939f9c89a1bc594b8267e2974c835f0f0
Christophe Delord <cdelord.fr>
2025-04-18 21:24:11 +0200

    update bang.luax

--- build.lua
+++ build.lua
@@ -18,7 +18,7 @@ For further information about luax you can visit
 https://codeberg.org/cdsoft/luax
 ]]
 
-version "8.9.2" "2025-04-18"
+version "8.9.3" "2025-04-18"
 
 local F = require "F"
 local fs = require "fs"
