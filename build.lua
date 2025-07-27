f776a97a22856d68d52e988afdeea67e51273522
Christophe Delord <cdelord.fr>
2025-07-27 17:30:05 +0200

    OpenSSL 3.5.1

--- build.lua
+++ build.lua
@@ -268,7 +268,7 @@ if not ssl then return end
 section "OpenSSL"
 ---------------------------------------------------------------------
 
-var "openssl_version" "3.5.0"
+var "openssl_version" "3.5.1"
 var "openssl_archive" "openssl-${openssl_version}.tar.gz"
 var "openssl_url" "https://github.com/openssl/openssl/releases/download/openssl-${openssl_version}/${openssl_archive}"
 
