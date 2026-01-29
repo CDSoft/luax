3a5f139f5147d26f966f8db78228364c4a48d644
Christophe Delord <cdelord.fr>
2026-01-29 21:25:09 +0100

    remove import cache management
    The cache is barely used and can be simulated by F.memo(import).

--- build.lua
+++ build.lua
@@ -18,7 +18,7 @@ For further information about luax you can visit
 https://codeberg.org/cdsoft/luax
 ]]
 
-version "9.11.1" "2026-01-13"
+version "9.11.2" "2026-01-30"
 
 local F = require "F"
 local fs = require "fs"
