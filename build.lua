9ea0eda5181ba42b46b43530336334aae590fb98
Christophe Delord <cdelord.fr>
2026-03-20 20:52:04 +0100

    revert to Lua 5.5.0 because of undefined behaviours in Lua 5.5.1

--- build.lua
+++ build.lua
@@ -18,7 +18,7 @@ For further information about luax you can visit
 https://codeberg.org/cdsoft/luax
 ]]
 
-version "9.14.5" "2026-03-19"
+version "9.14.6" "2026-03-20"
 
 local F = require "F"
 local fs = require "fs"
