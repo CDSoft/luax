fc92fc819dee6a8154cd3ad092db0c5b2b5fbb1f
Christophe Delord <cdelord.fr>
2026-07-23 20:40:13 +0200

    bang: allow multiple calls to the generator function
    e.g. to cumulate implicit inputs and outputs

--- luax/luax-version.lua
+++ luax/luax-version.lua
@@ -20,7 +20,7 @@ https://codeberg.org/cdsoft/luax
 
 --@LIB
 
-local version = "10.9"
+local version = "10.9.1"
 local year = 2026
 local url = "codeberg.org/cdsoft/luax"
 local author = "Christophe Delord"
