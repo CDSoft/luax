1739193a7232b569b05f9e08533d9b9815a64219
Christophe Delord <cdelord.fr>
2026-09-11 23:55:58 +0200

    luax 10.11.1

--- luax/luax-version.lua
+++ luax/luax-version.lua
@@ -22,7 +22,7 @@ https://codeberg.org/cdsoft/luax
 
 -- [tag:luax-version]
 
-local version = "10.11.0"
+local version = "10.11.1"
 local year = 2026
 local url = "codeberg.org/cdsoft/luax"
 local author = "Christophe Delord"
